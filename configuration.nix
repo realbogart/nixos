311a6fb8c207c606318388e53ecdfea1eadaad88
System administrator <root@nixos.>
2024-02-16 23:42:08 +0000

    Configured with flake

--- configuration.nix
+++ configuration.nix
@@ -10,7 +10,7 @@
 {
   imports = [
     # include NixOS-WSL modules
-    <nixos-wsl/modules>
+    # <nixos-wsl/modules>
   ];
 
   wsl.enable = true;
--- flake.nix
+++ flake.nix
@@ -4,22 +4,29 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    NixOS-WSL = {
+      url = "github:nix-community/NixOS-WSL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     # home-manager = {
     #   url = "github:nix-community/home-manager";
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, NixOS-WSL, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
       nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
           specialArgs = {inherit inputs;};
           modules = [ 
             ./configuration.nix
+	    NixOS-WSL.nixosModules.wsl
             # inputs.home-manager.nixosModules.default
           ];
         };
