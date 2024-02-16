eb0281760008e594eb7c966a45c4c8a24e5a2744
System administrator <root@nixos.>
2024-02-16 23:24:17 +0000

    Added flake.nix

--- configuration.nix
+++ configuration.nix
@@ -18,6 +18,10 @@
   
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  environment.systemPackages = with pkgs; [
+    git
+    neovim
+  ];
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
