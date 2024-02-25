189b302ac1c93b18e12f7c7eb54b58730068c0d3
Johan Yngman <johan.yngman@gmail.com>
2024-02-25 10:51:33 +0100

    Enable nix-command and flakes for desktop

--- configuration-desktop.nix
+++ configuration-desktop.nix
@@ -5,6 +5,8 @@
     [
       ./hardware-configuration.nix
     ];
+  
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
