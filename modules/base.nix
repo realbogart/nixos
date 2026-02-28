927b1d6cad5bc4268c4a2d08eadbc9f908117226
Johan Yngman <johan.yngman@gmail.com>
2026-02-28 16:35:24 +0100

    accept-flake-config = true

--- modules/base.nix
+++ modules/base.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 {
+  # Pin the Nix CLI to the latest version available in this nixpkgs.
+  nix.package = pkgs.nixVersions.latest;
   nix.settings.trusted-users = [ "root" "johan" ];
+  nix.settings.accept-flake-config = true;
   nix.settings.secret-key-files =
     [ "/home/johan/.config/nix/cache-priv-key.pem" ];
 
