a074084807e95d0dd061d29e96e07ec7a9f66f84
Johan Yngman <johan.yngman@gmail.com>
2026-02-28 16:40:01 +0100

    allow-import-from-derivation = true

--- modules/base.nix
+++ modules/base.nix
@@ -3,6 +3,7 @@
   # Pin the Nix CLI to the latest version available in this nixpkgs.
   nix.package = pkgs.nixVersions.latest;
   nix.settings.trusted-users = [ "root" "johan" ];
+  nix.settings.allow-import-from-derivation = true;
   nix.settings.accept-flake-config = true;
   nix.settings.secret-key-files =
     [ "/home/johan/.config/nix/cache-priv-key.pem" ];
